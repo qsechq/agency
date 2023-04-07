12e235fbfd07e3e35ee278ded5af8723f56f6bbd
Niita <ss16nik@gmail.com>
2023-04-07 16:26:33 +0400

    add modal

---
+++ src/components/ModalComponent/ModalComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="modal" class="modal" v-if="isShow">
+        <div @click.stop class="modal__content">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ModalComponent',
+    emits: [
+        'show'
+    ],
+    props: {
+        isShow: {
+            type: Boolean,
+            default: false
+        }
+    },
+    setup(props) {
+        console.log(props.isShow);
+
+    }
+}
+</script>
+
+<style lang="scss" scoped src="./modal-component.scss"></style>
--- src/components/SortComponent/SortComponent.vue
+++ src/components/SortComponent/SortComponent.vue
@@ -1,15 +1,78 @@
 <template>
     <div class="c-sort">
-        <a href="#" class="sort__btn">СНАЧАЛА ДОРОГИЕ</a>
+        <a @click.prevent="openModal()" href="#" class="sort__btn" ref="sort">СНАЧАЛА ДОРОГИЕ</a>
+        <teleport to="#sort">
+            <modal-component :isShow="isShow" class="modal__sort" ref="modal">
+                <template #content>
+                    <ul class="sort__list">
+                        <li class="sort__item">
+                            <button class="sort__btn">СНАЧАЛА ДОРОГИЕ</button>
+                        </li>
+                        <li class="sort__item">
+                            <button class="sort__btn">СНАЧАЛА НЕДОРОГИЕ</button>
+                        </li>
+                        <li class="sort__item">
+                            <button class="sort__btn">СНАЧАЛА ПОПУЛЯРНЫЕ</button>
+                        </li>
+                        <li class="sort__item">
+                            <button class="sort__btn">СНАЧАЛА НОВЫЕ</button>
+                        </li>
+                    </ul>
+                </template>
+            </modal-component>
+        </teleport>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'SortComponent'
+import { ModalComponent } from '../ModalComponent'
+// import { ref, onMounted, watchEffect } from 'vue';
+
+export default {
+    name: 'SortComponent',
+    components: {
+        ModalComponent
+    },
+    data() {
+        isShow: false
+    },
+    methods: {
+        openModal() {
+            this.isShow = true
+            let coords = this.$refs.sort.getBoundingClientRect()
+            let modal = document.querySelector('.modal__sort')
+            console.log(modal);
+            modal.style.left = coords.left + 'px'
+            modal.style.top = coords.bottom + 'px'
+            console.log(modal);
+        }
+    },
+    mounted() {
+        this.openModal()
     }
+    // setup() {
+    //     const isShow = ref(false)
+    //     const sort = ref(null)
+    //     const modal = ref(null)
+    //     const openModal = () => {
+    //         isShow.value = true
+    //         let coords = sort.value.getBoundingClientRect()
+    //         let modal = modal.value
+    //         console.log(modal);
+    //         modal.style.top = coords.bottom + "px"
+    //     }
+    //     onMounted(() => {
+    //         sort.value
+    //         modal.value
+    //     })
+    //     return {
+    //         isShow,
+    //         openModal,
+    //         sort,
+    //         modal
+    //     }
+    // }
+}
 </script>
 
-<style lang="scss" scoped src="./sort-component.scss">
-
-</style>
+<style lang="scss" scoped src="./sort-component.scss"></style>
